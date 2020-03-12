93367b455a87ab2e0502e14f3680550c7856ce95
Jooyi Park <jooyi1130@gmail.com>
2020-03-12 12:00:43 -0400

    added model for Suppliers

--- service/models.py
+++ service/models.py
@@ -1,5 +1,5 @@
 """
-Models for <your resource name>
+Models for Suppliers
 
 All of the models are stored in this module
 """
@@ -16,9 +16,9 @@ class DataValidationError(Exception):
     pass
 
 
-class YourResourceModel(db.Model):
+class Suppliers(db.Model):
     """
-    Class that represents a <your resource model name>
+    Class that represents a Suppliers
     """
 
     app = None
@@ -26,13 +26,16 @@ class YourResourceModel(db.Model):
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(63))
+    address = db.Column(db.String(130))
+    phone_number = db.Column(db.String(12))
+    #remember to make phone_number optional
 
     def __repr__(self):
-        return "<<your resource name> %r id=[%s]>" % (self.name, self.id)
+        return "<Suppliers %r id=[%s]>" % (self.name, self.id)
 
     def create(self):
         """
-        Creates a <your resource name> to the database
+        Creates a Suppliers to the database
         """
         logger.info("Creating %s", self.name)
         self.id = None  # id must be none to generate next primary key
@@ -41,38 +44,42 @@ class YourResourceModel(db.Model):
 
     def save(self):
         """
-        Updates a <your resource name> to the database
+        Updates a Suppliers to the database
         """
         logger.info("Saving %s", self.name)
         db.session.commit()
 
     def delete(self):
-        """ Removes a <your resource name> from the data store """
+        """ Removes a Suppliers from the data store """
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
-        """ Serializes a <your resource name> into a dictionary """
+        """ Serializes a Suppliers into a dictionary """
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "address": self.address,
+            "phone_number": self.phone_number,
         }
 
     def deserialize(self, data):
         """
-        Deserializes a <your resource name> from a dictionary
+        Deserializes a Suppliers from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
         """
         try:
             self.name = data["name"]
+            self.address = data["address"]
+            self.phone_number = data.get("phone_number")
         except KeyError as error:
-            raise DataValidationError("Invalid <your resource name>: missing " + error.args[0])
+            raise DataValidationError("Invalid Suppliers: missing " + error.args[0])
         except TypeError as error:
             raise DataValidationError(
-                "Invalid <your resource name>: body of request contained" "bad or no data"
+                "Invalid Suppliers: body of request contained" "bad or no data"
             )
         return self
 
@@ -88,28 +95,30 @@ class YourResourceModel(db.Model):
 
     @classmethod
     def all(cls):
-        """ Returns all of the <your resource name>s in the database """
-        logger.info("Processing all <your resource name>s")
+        """ Returns all of the Supplierss in the database """
+        logger.info("Processing all Supplierss")
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
-        """ Finds a <your resource name> by it's ID """
+        """ Finds a Suppliers by it's ID """
         logger.info("Processing lookup for id %s ...", by_id)
         return cls.query.get(by_id)
 
     @classmethod
     def find_or_404(cls, by_id):
-        """ Find a <your resource name> by it's id """
+        """ Find a Suppliers by it's id """
         logger.info("Processing lookup or 404 for id %s ...", by_id)
         return cls.query.get_or_404(by_id)
 
     @classmethod
     def find_by_name(cls, name):
-        """ Returns all <your resource name>s with the given name
+        """ Returns all Supplierss with the given name
 
         Args:
-            name (string): the name of the <your resource name>s you want to match
+            name (string): the name of the Supplierss you want to match
         """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
+
+    #whoever is doing the query story should create more query model
