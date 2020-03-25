138eb9ab984a112b565084641ffa0b98902d535f
Jooyi Park <jooyi1130@gmail.com>
2020-03-24 20:44:04 -0400

    added code for read-supplier

--- service/service.py
+++ service/service.py
@@ -19,13 +19,126 @@ from service.models import Suppliers, DataValidationError, Products
 from . import app
 
 ######################################################################
+# Error Handlers
+######################################################################
+@app.errorhandler(DataValidationError)
+def request_validation_error(error):
+    """ Handles Value Errors from bad data """
+    return bad_request(error)
+
+
+@app.errorhandler(status.HTTP_400_BAD_REQUEST)
+def bad_request(error):
+    """ Handles bad reuests with 400_BAD_REQUEST """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(
+            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
+        ),
+        status.HTTP_400_BAD_REQUEST,
+    )
+
+
+@app.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found(error):
+    """ Handles resources not found with 404_NOT_FOUND """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
+        status.HTTP_404_NOT_FOUND,
+    )
+
+
+@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
+def method_not_supported(error):
+    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            error="Method not Allowed",
+            message=message,
+        ),
+        status.HTTP_405_METHOD_NOT_ALLOWED,
+    )
+
+
+@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+def mediatype_not_supported(error):
+    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(
+            status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            error="Unsupported media type",
+            message=message,
+        ),
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+    )
+
+
+@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """ Handles unexpected server error with 500_SERVER_ERROR """
+    message = str(error)
+    app.logger.error(message)
+    return (
+        jsonify(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            error="Internal Server Error",
+            message=message,
+        ),
+        status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
+
+######################################################################
 # GET INDEX
 ######################################################################
 @app.route("/")
 def index():
     """ Root URL response """
-    return "Reminder: return some useful information in json format about the service here", status.HTTP_200_OK
+    return (
+        jsonify(
+            name="Account REST API Service",
+            version="1.0",
+            paths=url_for("list_suppliers", _external=True),
+        ),
+        status.HTTP_200_OK,
+    )
 
+######################################################################
+# LIST ALL suppliers
+######################################################################
+@app.route("/suppliers", methods=["GET"])
+def list_suppliers():
+    """ Returns all of the suppliers """
+    app.logger.info("Request for Supplier list")
+    suppliers = []
+    name = request.args.get("name")
+    if name:
+        suppliers = Suppliers.find_by_name(name)
+    else:
+        suppliers = Suppliers.all()
+
+    results = [supplier.serialize() for supplier in suppliers]
+    return make_response(jsonify(results), status.HTTP_200_OK)
+
+######################################################################
+# RETRIEVE A SUPPLIER
+######################################################################
+@app.route("/suppliers/<int:supplier_id>", methods=["GET"])
+def get_suppliers(supplier_id):
+    """
+    Retrieve a single Supplier
+    This endpoint will return a Supplier based on it's id
+    """
+    app.logger.info("Request for Supplier with id: %s", supplier_id)
+    supplier = Suppliers.find_or_404(supplier_id)
+    return make_response(jsonify(supplier.serialize()), status.HTTP_200_OK)
 
 ######################################################################
 # CREATE A NEW SUPPLIER
@@ -43,7 +156,7 @@ def create_suppliers():
     supplier.create()
     message = supplier.serialize()
     #TODO: when get_suppliers (READ SUPPLIER) is implemented , remove the commented code below
-    #location_url = url_for("get_suppliers", supplier_id=supplier.id, _external=True)
+    # location_url = url_for("get_suppliers", supplier_id=supplier.id, _external=True)
     location_url = "not implemented"
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
--- tests/test_service.py
+++ tests/test_service.py
@@ -50,9 +50,67 @@ class TestYourResourceServer(TestCase):
         db.drop_all()
 
 ######################################################################
+#  H E L P E R   M E T H O D S
+######################################################################
+
+    def _create_suppliers(self, count):
+        """ Factory method to create suppliers in bulk """
+        suppliers = []
+        for _ in range(count):
+            supplier = SupplierFactory()
+            resp = self.app.post(
+                "/suppliers", json=supplier.serialize(), content_type="application/json"
+            )
+            self.assertEqual(
+                resp.status_code, status.HTTP_201_CREATED, "Could not create test Supplier"
+            )
+            new_supplier = resp.get_json()
+            supplier.id = new_supplier["id"]
+            suppliers.append(supplier)
+        return suppliers
+
+######################################################################
 #  P L A C E   T E S T   C A S E S   H E R E 
 ######################################################################
 
+    def test_index(self):
+        """ Test index call """
+        resp = self.app.get("/")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_get_supplier_list(self):
+        """ Get a list of suppliers """
+        self._create_suppliers(5)
+        resp = self.app.get("/suppliers")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_get_supplier_by_name(self):
+        """ Get a supplier by Name """
+        suppliers = self._create_suppliers(3)
+        resp = self.app.get("/suppliers?name={}".format(suppliers[1].name))
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data[0]["name"], suppliers[1].name)
+
+    def test_get_supplier(self):
+        """ Get a single supplier """
+        # get the id of an supplier
+        supplier = self._create_suppliers(1)[0]
+        resp = self.app.get(
+            "/suppliers/{}".format(supplier.id), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], supplier.name)
+
+    def test_get_supplier_not_found(self):
+        """ Get an supplier that is not found """
+        resp = self.app.get("/suppliers/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+       
     def test_index(self):
         """ Test index call """
         resp = self.app.get("/")
@@ -71,6 +129,7 @@ class TestYourResourceServer(TestCase):
         # Make sure location header is set
         location = resp.headers.get("Location", None)
         self.assertIsNotNone(location)
+        app.logger.debug(location)
         
         # Check the data is correct
         new_supplier = resp.get_json()
@@ -83,12 +142,12 @@ class TestYourResourceServer(TestCase):
 
         #TODO: when get_supplier (READ SUPPLIER) is implemented , remove the commented code below
         # # Check that the location header was correct by getting it
-        # resp = self.app.get(location, content_type="application/json")
-        # self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        # new_supplier = resp.get_json()
-        # self.assertEqual(new_supplier["name"], supplier.name, "Names does not match")
-        # self.assertEqual(new_supplier["category"], supplier.category, "Category does not match")
-        # self.assertEqual(new_supplier["address"], supplier.address, "Address does not match")
-        # self.assertEqual(new_supplier["email"], supplier.email, "Email does not match")
-        # self.assertEqual(new_supplier["phone_number"], supplier.phone_number, "Phone does not match")
-        # self.assertEqual(new_supplier["products"], str(supplier.products), "Products does not match")
+        #resp = self.app.get(location, content_type="application/json")
+        #self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        #new_supplier = resp.get_json()
+        #self.assertEqual(new_supplier["name"], supplier.name, "Names does not match")
+        #self.assertEqual(new_supplier["category"], supplier.category, "Category does not match")
+        #self.assertEqual(new_supplier["address"], supplier.address, "Address does not match")
+        #self.assertEqual(new_supplier["email"], supplier.email, "Email does not match")
+        #self.assertEqual(new_supplier["phone_number"], supplier.phone_number, "Phone does not match")
+        #self.assertEqual(new_supplier["products"], str(supplier.products), "Products does not match")
